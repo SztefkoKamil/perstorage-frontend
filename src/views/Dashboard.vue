6927c13fa80c6424a22d0388fa30b4c36c15f184
Kamil Sztefko <sztefkokamil@gmail.com>
2019-12-18 21:45:01 +0100

    Add styles & Panel
    Added basic styles.
    Added Panel component.

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,14 +1,27 @@
 <template>
-  <div class="home">
+  <div class="dashboard">
+    <Panel></Panel>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Panel from '../components/Dashboard/Panel';
 
 export default {
   name: 'dashboard',
   components: {
+    Panel
   }
 }
 </script>
+
+<style lang="scss" scoped>
+
+.dashboard {
+  width: 100%;
+  height: 600px;
+  background: hsla(210, 5%, 29%, 45%);
+}
+
+</style>
