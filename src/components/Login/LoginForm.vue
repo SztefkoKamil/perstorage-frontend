16cbaea83b3b89e36592625a281463bca38bc268
Kamil Sztefko <sztefkokamil@gmail.com>
2019-12-21 21:32:38 +0100

    Add LoginForm
    Added LoginForm component with basic styles.

---
+++ src/components/Login/LoginForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <form class="login-form">
+    <label for="login-email">email</label>
+    <input type="email" name="" id="login-email">
+    <label for="login-pass">password</label>
+    <input type="password" name="" id="login-pass">
+    <button type="submit">Login</button>
+  </form>
+</template>
+
+<script>
+
+export default {
+  name: 'login',
+}
+</script>
+
+<style lang="scss" scoped>
+
+.login-form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px 0 50px;
+  border-bottom: 1px dashed #fff;
+
+  label {
+    text-align: left;
+    font-size: 18px;
+    margin: 20px 0 2px;;
+  }
+  input {
+    width: 300px;
+    font-size: 18px;
+    color: hsl(210, 65%, 28%);
+    text-align: center;
+    border-radius: 5px;
+    padding: 4px 6px;
+    background: #fff;
+    border: 2px solid hsl(210, 65%, 28%);
+  }
+  button {
+    margin-top: 30px;
+    border: 2px solid hsl(210, 65%, 28%);
+    border-radius: 5px;
+    padding: 10px 30px;
+    font-size: 20px;
+    background: #fff;
+    color: hsl(210, 65%, 28%);
+    cursor: pointer;
+  }
+}
+
+</style>
