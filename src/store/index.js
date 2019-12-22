7b36eecb944d886c1c58eebc7ef9a5959fcc5e8c
Kamil Sztefko <sztefkokamil@gmail.com>
2019-12-22 20:05:54 +0100

    Add postLogin to VUEX
    Added postLogin action without redirect to dashboard.

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import secret from '../../secret'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -9,6 +11,28 @@ export default new Vuex.Store({
   mutations: {
   },
   actions: {
+    postLogin: async (context, user) => {
+      const route = secret.mainRoute + secret.loginRoute;
+      const request = {
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: { 'Content-Type': 'Application/json' }
+      };
+
+      try {
+        const response = await fetch(route, request)
+        if(response.status !== 200) {
+          throw new Error('Login filed');
+        }
+        const result = await response.json();
+        
+        localStorage.setItem('token', result.token);
+        localStorage.setItem('userId', result.userId);
+
+        // redirect to dashboard view
+
+      } catch (err) { console.log(err.message); }
+    }
   },
   modules: {
   }
