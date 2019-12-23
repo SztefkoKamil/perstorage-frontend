fcec8caaa2ae8b527f3b5c8180462f8fc0ed80ae
Kamil Sztefko <sztefkokamil@gmail.com>
2019-12-23 15:53:18 +0100

    Add VUEX postSignup

--- src/store/index.js
+++ src/store/index.js
@@ -25,7 +25,7 @@ export default new Vuex.Store({
         if(response.status !== 202) {
           console.log(result);
           // call showError VUEX action
-          throw new Error('Login filed');
+          throw new Error('Login failed');
         }
         console.log(result);
         
@@ -33,6 +33,28 @@ export default new Vuex.Store({
         localStorage.setItem('userId', result.userId);
 
         // redirect to dashboard view
+
+      } catch (err) { console.log(err.message); }
+    },
+    postSignup: async (context, newUser) => {
+      const route = secret.mainRoute + secret.signupRoute;
+      const config = {
+        method: 'POST',
+        body: JSON.stringify(newUser),
+        headers: { 'Content-Type': 'Application/json' }
+      };
+
+      try {
+        const response = await fetch(route, config);
+        const result = await response.json();
+        if(response.status !== 201) {
+          console.log(result);
+          // call showError VUEX action
+          throw new Error('Signup failed');
+        }
+        console.log(result);
+
+        // redirect to login view
 
       } catch (err) { console.log(err.message); }
     }
