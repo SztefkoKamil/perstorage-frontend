4de8213098f27cd2ca965ccbc3778345758c5a0d
Kamil Sztefko <sztefkokamil@gmail.com>
2020-02-16 08:27:09 +0100

    Add Dashboard info

--- src/components/Dashboard/Panel.vue
+++ src/components/Dashboard/Panel.vue
@@ -6,6 +6,16 @@
       <button @click="logout">logout</button>
     </div>
     <UploadForm></UploadForm>
+    <div class="panel-info">
+      <transition name="toggle-info">
+        <p v-if="toggleInfo">
+          <span>Enabled file formats: jpeg, jpg, png, pdf, txt, doc, docx, rar, zip, 7z.</span>
+          <span>File maximum size: 5Mb</span>
+          <span>Maximum number of stored files: 20</span>
+        </p>
+      </transition>
+      <button @click="toggleInfo = !toggleInfo">?</button>
+    </div>
     <div class="toggleBtn-wrapper">
       <button 
         @click="isPanelOpen = !isPanelOpen" 
@@ -19,20 +29,19 @@
 </template>
 
 <script>
-import UploadForm from './UploadForm'
-import { eventBus } from '../../main'
+import UploadForm from './UploadForm';
+import { eventBus } from '../../main';
 import { mapState } from 'vuex';
 
 export default {
   components: { UploadForm },
-  data() {
-    return {
+  data() { return {
+      toggleInfo: false,
       navContainer: null,
       isPanelOpen: false,
-      info: 'Hello, add some files',
+      info: 'Hello, add some files.',
       filesCounter: 0
-    }
-  },
+    }; },
   methods: {
     logout() {
       localStorage.removeItem('token');
@@ -57,7 +66,7 @@ export default {
   mounted() {
     this.navContainer = document.getElementById('panel');
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -105,6 +114,60 @@ export default {
     font-size: 14px;
     padding: 6px 17px;
     margin: 0 10px;
+    cursor: pointer;
+  }
+}
+
+.panel-info {
+  position: absolute;
+  top: 100px;
+  left: -30px;
+  width: 300px;
+  @include flexRow;
+
+  p {
+    position: absolute;
+    width: 300px;
+    color: #fff;
+    background: $colorOne;
+    padding: 10px;
+    border-radius: 5px;
+    border: 1px solid $colorTwo;
+    left: 40px;
+    top: 20px;
+    line-height: 120%;
+    text-align: left;
+    @include flexColumn(center, flex-start);
+
+    span:nth-child(2) {
+      margin: 5px 0;
+    }
+  }
+  .toggle-info-enter-active {
+    animation: show-panel-info .1s 1 linear;
+  }
+  @keyframes show-panel-info {
+    0% { opacity: 0; }
+    100% { opacity: 1; }
+  }
+  .toggle-info-leave-active {
+    animation: hide-panel-info .1s 1 linear;
+  }
+  @keyframes hide-panel-info {
+    0% { opacity: 1; }
+    100% { opacity: 0; }
+  }
+
+  button {
+    @include flexRow;
+    width: 20px;
+    height: 20px;
+    background: #fff;
+    border: none;
+    border-radius: 50%;
+    font-size: 18px;
+    font-weight: 900;
+    color: $colorOne;
     cursor: pointer;
   }
 }
