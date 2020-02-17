139a3dcba5a7561817d774a0fb7cf4fdad78b141
Kamil Sztefko <sztefkokamil@gmail.com>
2020-02-17 15:13:26 +0100

    Add info to Signup.vue

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="signup main-view">
     <h1>Signup to Perstorage</h1>
+    <div class="signup-info">
+      <transition name="toggle-info">
+        <p v-if="toggleInfo">
+          <span>Users number limited to 5. If it's full contact me at sztefkokamil@gmail.com</span>
+          <span>Name must have min 2 characters length.</span>
+          <span>Password must have min 6 characters length.</span>
+        </p>
+      </transition>
+      <button @click="toggleInfo = !toggleInfo">?</button>
+    </div>
     <SignupForm></SignupForm>
   </div>
 </template>
@@ -12,13 +22,16 @@ export default {
   name: 'home',
   components: {
     SignupForm
-  }
-}
+  },
+  data() { return {
+    toggleInfo: false
+  }; }
+};
 </script>
 
 <style lang="scss" scoped>
-// @import "../scss/variables.scss";
-// @import "../scss/flexMixins.scss";
+@import "../scss/variables.scss";
+@import "../scss/flexMixins.scss";
 
 .signup {
   h1 {
@@ -34,6 +47,73 @@ export default {
     h1 {
       font-size: 32px;
     }
+  }
+}
+
+.signup-info {
+  margin: 30px 0 20px;
+  position: relative;
+  width: 360px;
+  @include flexRow;
+
+  p {
+    position: absolute;
+    width: 320px;
+    color: #fff;
+    background: $colorOne;
+    padding: 10px;
+    border-radius: 5px;
+    left: 20px;
+    top: 40px;
+    line-height: 120%;
+
+    span {
+    display: block;
+    text-align: left;
+
+    &:nth-child(2) {
+      margin: 7px 0;
+    }
+  }
+  }
+  .toggle-info-enter-active {
+    animation: show-info .1s 1 linear;
+  }
+  @keyframes show-info {
+    0% {
+      opacity: 0;
+      transform: translateY(-20px);
+    }
+    100% {
+      opacity: 1;
+      transform: translateY(0px);
+    }
+  }
+  .toggle-info-leave-active {
+    animation: hide-info .1s 1 linear;
+  }
+  @keyframes hide-info {
+    0% {
+      opacity: 1;
+      transform: translateY(0px);
+    }
+    100% {
+      opacity: 0;
+      transform: translateY(-20px);
+    }
+  }
+
+  button {
+    @include flexRow;
+    width: 24px;
+    height: 24px;
+    background: #fff;
+    border: none;
+    border-radius: 50%;
+    font-size: 20px;
+    font-weight: 900;
+    color: $colorOne;
+    cursor: pointer;
   }
 }
 
