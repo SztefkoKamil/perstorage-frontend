0e0e00d3395f77bdda24cb8f05d9947d8e4d3481
Kamil Sztefko <sztefkokamil@gmail.com>
2020-03-08 09:09:15 +0100

    Update secret-sample.js

--- secret-example.js
+++ secret-example.js
@@ -1,26 +1,25 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  mainRoute: 'host',
+  mainRoute: '',
 
-  // user routes
-  loginRoute: 'route',
-  signupRoute: 'route',
-  deleteUser: 'route',
+  loginRoute: '',
+  signupRoute: '',
+  deleteUser: '',
 
-  // files routes
-  getFiles: 'route',
-  postFiles: 'route',
-  putFile: 'route',
-  deleteFile: 'route',
+  getFiles: '',
+  postFiles: '',
+  downloadFile: '',
+  putFile: '',
+  deleteFile: '',
 
-  // file types
   fileTypes: [
     // image - jpeg, jpg, png
     'image/jpeg', 'image/jpg', 'image/png', 
 
     // compressed - zip, 7z, rar
-    'application/zip', 'application/x-7z-compressed', 'application/x-rar-compressed', 
+    'application/zip', 'application/x-zip-compressed', 'application/x-7z-compressed', 'application/x-rar-compressed', 
 
     // document - pdf, txt, doc, docx
     'application/pdf', 'text/plain', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
   ]
-}
+};
