ed9afcda9c23a721dc5ac44127b8908f4307551f
Kamil Sztefko <sztefkokamil@gmail.com>
2020-05-06 20:13:45 +0200

    Refactor tests for /components/Login/LoginForm.vue
    One big test case with many expectations for 4 component elements
    splitted to 4 small test cases.

--- src/test/Components/Login/LoginForm.test.js
+++ src/test/Components/Login/LoginForm.test.js
@@ -3,30 +3,51 @@ import Vuex from 'vuex';
 import LoginForm from '../../../components/Login/LoginForm.vue';
 
 describe('/components/Login/LoginForm.vue', () => {
-  it('should contains form with required email & password inputs & submit button', () => {
+  it('should contain form', () => {
     const wrapper = shallowMount(LoginForm);
     const form = wrapper.find('form');
-    const email = form.find('input[type="email"]');
-    const password = form.find('input[type="password"');
-    const button = form.find('button[type="submit"');
 
     expect(form.exists()).toBe(true);
+  });
+
+  it('form should contain input id="login-email" type="email" required', () => {
+    const wrapper = shallowMount(LoginForm);
+    const form = wrapper.find('form');
+    const email = form.find('input#login-email');
+
     expect(email.exists()).toBe(true);
+    expect(email.attributes().type).toBe('email');
     expect(email.attributes().required).toBe('required');
+  });
+
+  it('form should contain input id="login-pass" type="password" required', () => {
+    const wrapper = shallowMount(LoginForm);
+    const form = wrapper.find('form');
+    const password = form.find('input#login-pass');
+
     expect(password.exists()).toBe(true);
+    expect(password.attributes().type).toBe('password');
     expect(password.attributes().required).toBe('required');
+  });
+
+  it('form should contain button type="submit"', () => {
+    const wrapper = shallowMount(LoginForm);
+    const form = wrapper.find('form');
+    const button = form.find('button');
+
     expect(button.exists()).toBe(true);
+    expect(button.attributes().type).toBe('submit');
   });
 
   it('form submit should call postLogin VUEX action with proper data', () => {
     const localVue = createLocalVue();
     localVue.use(Vuex);
     const actions = { postLogin: jest.fn((arg1, arg2) => {}) };
-    const store = new Vuex.Store({actions});
-    const wrapper = shallowMount(LoginForm, {store, localVue});
+    const store = new Vuex.Store({ actions });
+    const wrapper = shallowMount(LoginForm, { store, localVue });
     const data = {
       email: 'test@test.com',
-      password: 'password1'
+      password: 'password1',
     };
 
     wrapper.find('input[type="email"]').setValue(data.email);
